ea944cc907f6d2433a8bc818ec84e1e3a834e376
Leila <leiladossantos@yahoo.com>
2020-01-29 16:27:53 +0000

    Solution week1

--- challenges/week1.js
+++ challenges/week1.js
@@ -15,39 +15,67 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  let vat = ((vatRate/100)*originalPrice)+originalPrice;
+  return +vat.toFixed(2);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  let finalSalePrice =  originalPrice - (reduction/ 100)* originalPrice;
+  return +finalSalePrice.toFixed(2);
+
 }
+
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  const middle = Math.floor(str.length/2);
+    if (str.length % 2 == 0) {
+        return str[middle-1] + str[middle];
+}else
+        return str[middle] 
 }
+
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.split("").reverse().join("");
+
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  let reverse = [];
+  for(let i in words){
+    let word = words[i].split("").reverse().join("");
+    reverse.push(word);
+  }
+  return reverse;
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  // Add your code here!
+  let counter = 0;
+  for (const input of users) {
+    if (input.status == 0) counter += 1;
+  }
+  return users.length;
 }
+
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
+  let sum = 0;
+    for(const i in scores) {
+        sum += scores[i];
+    }
+   
+    let numbers = scores.length;
+  
+    return (sum / numbers).toFixed(0,2);
 }
+
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
