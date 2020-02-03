b01f6f9671d735eca7435bcd197800fe997a886f
Leila <leiladossantos@yahoo.com>
2020-02-03 00:01:46 +0000

    solutions for challenges/week2.js

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,16 +1,21 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.city === "Manchester") {
+    return true
+  }else {
+    return false
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  ("people is required");
+return (Math.ceil(people/40));
 }
 
 function countSheep(arr) {
